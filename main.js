9d5e21c84ce1a5833da055a8e43d402f5cfe553e
erenova <erenova6@gmail.com>
2023-09-05 13:17:29 +0300

    add player structure

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ const PlayBoard = (() => {
     _currentTurn = _currentTurn === "1" ? "2" : "1";
   };
   const findTurn = () => {
-    return _currentTurn;
+    let whoseTurn = _currentTurn === "1" ? "X" : "O";
+    return whoseTurn;
   };
   const makeMove = (moveIndex) => {
     if (moveIndex >= _board.length || _board[moveIndex] !== "-") {
@@ -28,11 +29,11 @@ const PlayBoard = (() => {
     if (_movesLeft.includes(`${moveIndex}`)) {
       let tempIndex = _movesLeft.findIndex((e) => e === `${moveIndex}`);
       _movesLeft.splice(tempIndex, 1);
-      _board[moveIndex] = _currentTurn === "1" ? "X" : "O";
+      _board[moveIndex] = findTurn();
       _switchTurn();
       console.log(_movesLeft);
       getAsConsoleBoard();
-      _checkWinPattern();
+      _decideGameState(getBoard());
       return true; // Valid
     }
   };
@@ -43,12 +44,12 @@ const PlayBoard = (() => {
   };
   const getBoard = () => [..._board];
 
+  const getMovesLeft = () => [..._movesLeft];
+
   const getIndexValue = (index) => [..._board][index];
 
-  const getMovesLeft = () => [..._movesLeft];
-  const stopGame = () => {
+  const _stopGame = () => {
     _winnerState.winnerPattern = ``;
-
     _movesLeft = [];
     _currentTurn = "1";
     console.log("Game Ended!");
@@ -56,57 +57,64 @@ const PlayBoard = (() => {
 
   const resetGame = () => {
     _winnerState.winnerSign = ``;
-    stopGame();
+    _stopGame();
     _movesLeft = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 
     _board.fill("-");
     console.log("New Game Started!");
   };
   /* ["012", "345", "678", "036", "147", "258", "048", "246"] */
-  const _checkWinPattern = () => {
-    let isGameEnded = false;
+
+  const _checkWinPattern = (gameboard) => {
     for (let i = 0; i < _winPatterns.length; i++) {
-      if (isGameEnded === false) {
-        //Possible win scenario
-        let patterns = [
-          _winPatterns[i][0],
-          _winPatterns[i][1],
-          _winPatterns[i][2],
-        ];
-        //Is that scenario filled correctly in the board?
-        let activeValues = [
-          _board[patterns[0]],
-          _board[patterns[1]],
-          _board[patterns[2]],
-        ];
-        let activeSign = activeValues[0];
-        //If So?
-        if (
-          activeSign !== "-" &&
-          activeSign === activeValues[1] &&
-          activeSign === activeValues[2]
-        ) {
-          _winnerState.wonPattern = `${patterns[0]}${patterns[1]}${patterns[2]}`;
-          _winnerState.winnerSign = activeSign;
-          console.log(
-            `Winner Pattern:${_winnerState.wonPattern} Winner Sign: ${activeSign}`
-          );
-          stopGame();
-          isGameEnded = true;
-          return;
-        }
+      let patterns = [
+        _winPatterns[i][0],
+        _winPatterns[i][1],
+        _winPatterns[i][2],
+      ];
+      let activeValues = [
+        gameboard[patterns[0]],
+        gameboard[patterns[1]],
+        gameboard[patterns[2]],
+      ];
+      let activeSign = activeValues[0];
+      if (
+        activeSign !== "-" &&
+        activeSign === activeValues[1] &&
+        activeSign === activeValues[2]
+      ) {
+        return {
+          sign: activeSign,
+          pattern: `${patterns[0]}${patterns[1]}${patterns[2]}`,
+        };
       }
     }
-    gameTieState();
+    return null;
   };
 
-  const gameTieState = () => {
+  const _decideGameState = (gameboard) => {
+    const winner = _checkWinPattern(gameboard);
+    if (winner !== null) {
+      _winnerState.winnerSign = winner.sign;
+      _winnerState.winnerPattern = winner.pattern;
+      console.log(
+        `Winner Sign: ${winner.sign} Winner Pattern: ${winner.pattern}`
+      );
+      _stopGame();
+    } else {
+      _gameTieState();
+    }
+  };
+
+  const _gameTieState = () => {
     if (_movesLeft.length === 0 && _winnerState.winnerSign === "") {
-      stopGame();
+      _stopGame();
       console.log("Game is tie");
     }
     return;
   };
+
+  const minimax = (board, sign) => {};
 
   const getAsConsoleBoard = () => {
     console.log(
@@ -133,8 +141,18 @@ const PlayBoard = (() => {
     makeRandomMove,
     getIndexValue,
     getMovesLeft,
-    stopGame,
     resetGame,
     getAsConsoleBoard,
   });
 })();
+
+const createPlayer = (name, state = "player") => {
+  if (state !== "player" || state !== "ai") state = "player";
+  const playerName = name;
+  const playerState = state;
+
+  return Object.freeze({ playerName, playerState });
+};
+
+const PlayerOne = createPlayer("eren", "ai");
+const PlayerTwo = createPlayer("erenova", "ai");
